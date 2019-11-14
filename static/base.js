d526a4d7497e6e0402be786b74492f3482a7a614
zxy <z5137425@cse.unsw.edu.au>
2019-11-15 05:55:10 +1100

    fixed time and state displaying

--- static/base.js
+++ static/base.js
@@ -7,3 +7,52 @@ function utcToDateString(utc) {
     return date + " " + time;
 }
 
+
+function immutubleElemTimeTransfer(elem, field) {
+    let time = elem[field];
+    time = parseInt(time);
+    if (time !== NaN) {
+        elem[field] = utcToDateString(time);
+    }
+}
+
+function mutubleElemTimeTransfer(elem, field) {
+    let visible = elem.cloneNode(false);
+    elem.display = "none";
+    elem.disabled = true;
+    let v_id = elem.id
+    if (v_id !== "") {
+        visible.id = "";
+    }
+    visible.name = "";
+    elem.parentNode.insertBefore(visible, elem.nextSibling);
+    visible[field] = utcToDateString(parseInt(elem[field]));
+    visible.addEventListener("change", (event) => {
+        let date = new Date(visible[field]);
+        let new_utc = date.getTime() / 1000;
+        console.log(new_utc);
+        if (!isNaN(new_utc)) {
+            console.log(new_utc);
+            elem[field] = new_utc;
+        }
+        else {
+            //event.preventDefault();
+            visible[field] = utcToDateString(parseInt(elem[field]));
+        }
+    });
+}
+
+let state_table = {
+    "1": "in inventory",
+    "2": "testing",
+    "3": "used",
+    "4": "disposed"
+}
+let test_state_table = {
+    "1": "not tested",
+    "2": "Good blood",
+    "3": "Bad blood"
+}
+
+
+
